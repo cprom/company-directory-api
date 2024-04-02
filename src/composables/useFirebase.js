3ae6715288b50e749e3422edb497494985cf76cd
Chen Prom <chen.prom@gmail.com>
2024-04-01 20:00:00 -0500

    Initial commit

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAp-6CAdM73cvcQ8pI_sFk4Bs80M76kS50",
+  authDomain: "company-directory-14e7f.firebaseapp.com",
+  projectId: "company-directory-14e7f",
+  storageBucket: "company-directory-14e7f.appspot.com",
+  messagingSenderId: "1006947712705",
+  appId: "1:1006947712705:web:f10babb3dacb74fd795e40"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
+const title = ref('Hello There ✋')
+</script>
+
+<template>
+<MainCards/>
+<MainPagination></MainPagination>
+</template>
